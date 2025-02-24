c9eb16c4453b5b1f3a45d979e4d8f74844960262
Marckenley Dorsainvil <marckenlygbeau@gmail.com>
2025-02-24 19:40:46 +0400

    Remove .env files from repository

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -220,7 +220,7 @@ const setAuthToken = (res: Response, userId: string) => {
     res.cookie("auth_token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 86400000, // 1 day in milliseconds
     });
   
